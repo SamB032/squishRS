74e1e07c011ec86d0e04c364b8ef3b28b18ab182
Sam <sam.boffey04@gmail.com>
2026-02-17 12:11:47 +0000

    fix: decending issue

--- src/cmd/mod.rs
+++ src/cmd/mod.rs
@@ -147,7 +147,7 @@ pub fn build_list_summary_table(summary: &ArchiveSummary) -> String {
 
     // Sort directories by file count descending
     let mut dir_counts_vec: Vec<_> = dir_counts.into_iter().collect();
-    dir_counts_vec.sort_by(|a, b| b.1.cmp(&a.1));
+    dir_counts_vec.sort_by_key(|x| Reverse(x.1));
 
     for (dir, count) in dir_counts_vec {
         breakdown_table.add_row(row![dir, count.to_formatted_string(&Locale::en)]);
